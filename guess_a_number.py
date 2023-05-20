d2ed65641be299a6175b82101d3b7991a3d9213b
hrisigl <128967457+hrisigl@users.noreply.github.com>
2023-05-20 09:27:45 +0300

    added attempts to the project

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,15 +2,25 @@ import random
 
 computer_guess = random.randint(1, 100)
 
+print("You have 5 attempts to guess the number!")
+
+attempts_counter = 0
 is_guess_right = False
+
 while not is_guess_right:
     player_guess = input("Guess the number (1-100): ")
+    if attempts_counter == 5:
+        print("You don't have more attempts!")
+        break
+    attempts_counter += 1
 
     if not player_guess.isdigit():
         print("Invalit Input. Try again...")
+        print(f"You have {5 - attempts_counter} attempts left.")
         continue
 
     player_guess = int(player_guess)
+
     if player_guess == computer_guess:
         print("You win!")
         is_guess_right = True
@@ -19,4 +29,4 @@ while not is_guess_right:
         print("Too low!")
     else:
         print("Too high!")
-
+    print(f"You have {5 - attempts_counter} attempts left.")
